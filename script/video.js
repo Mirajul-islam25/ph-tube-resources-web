cd6df331a8d21e5005e500cf47d4a392ae104b68
Mirajul <islammirajul150978@gmail.com>
2024-10-03 15:46:08 +0600

    ph-tube

---
+++ script/video.js
@@ -0,0 +1,8 @@
+// console.log('added link');
+
+// 1/1 - Fetch, Load and Show Categories on html
+//create loadCategories
+const loadCategories = () =>{
+      fetch('')
+}
+//create displayCategories
